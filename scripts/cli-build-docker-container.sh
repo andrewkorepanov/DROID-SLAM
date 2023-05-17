e3c30cfe27f8b93fff79fb0f86992d2cdf4ef168
andrew.korepanov <andrew.korepanov@imotions.com>
2023-05-17 10:36:02 +0200

    docker file renamed

--- scripts/cli-build-docker-container.sh
+++ scripts/cli-build-docker-container.sh
@@ -4,4 +4,4 @@ source $(dirname -- "$0")/app-constants.sh
 echo $DOCKER_CONTAINER_IMAGE_NAME
 docker build \
     --tag=$DOCKER_CONTAINER_IMAGE_NAME:$DOCKER_CONTAINER_IMAGE_TAG \
-    --file Dockerfile .
+    --file dockerfile .
